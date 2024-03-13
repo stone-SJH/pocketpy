a79d97a9a53621077757954831bec139f00e6842
Stone <stone.shijiahao@gmail.com>
2024-03-13 12:43:40 +0800

    fix lut error

--- src/pocketpy_c.cpp
+++ src/pocketpy_c.cpp
@@ -632,11 +632,11 @@ void replace_all(std::string& str, const std::string& from, const std::string& t
 void pkpy_compile_to_string(pkpy_vm* vm_handle, const char* source, const char* filename, int mode, bool unknown_global_scope, bool* ok, char** out) {
     VM* vm = (VM*)vm_handle;
     pkpy_clear_error(vm_handle, NULL);
-
+    std::string decoded = base64_decode(source);
     try {
-        std::string decoded = base64_decode(source);
+        
 
-        std::vector<std::string> s_20_7f = { " ", "!", "\"", "#", "$", " % ", " & ", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", ""};
+        std::vector<std::string> s_20_7f = { " ", "!", "\"", "#", "$", "%", "&", /*"'"*/ "\\'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", /*"\\"*/"\\\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~", ""};
         std::vector<std::string> s_80_ff = {"€", "", "‚", "ƒ", "„", "…", "†", "‡", "ˆ", "‰", "Š", "‹", "Œ", "", "Ž", "", "", "‘", "’", "“", "”", "•", "–", "—", "˜", "™", "š", "›", "œ", "", "ž", "Ÿ", " ", "¡", "¢", "£", "¤", "¥", "¦", "§", "¨", "©", "ª", "«", "¬", "­", "®", "¯", "°", "±", "²", "³", "´", "µ", "¶", "·", "¸", "¹", "º", "»", "¼", "½", "¾", "¿", "À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "Ç", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï", "Ð", "Ñ", "Ò", "Ó", "Ô", "Õ", "Ö", "×", "Ø", "Ù", "Ú", "Û", "Ü", "Ý", "Þ", "ß", "à", "á", "â", "ã", "ä", "å", "æ", "ç", "è", "é", "ê", "ë", "ì", "í", "î", "ï", "ð", "ñ", "ò", "ó", "ô", "õ", "ö", "÷", "ø", "ù", "ú", "û", "ü", "ý", "þ", "ÿ"};
 
         std::regex pattern("\\\\x([0-9A-Fa-f]{2})");
@@ -655,7 +655,7 @@ void pkpy_compile_to_string(pkpy_vm* vm_handle, const char* source, const char* 
         for (int i = 0; i < to_replace.size(); i++) {
             std::string hex = to_replace[i];
             int decimal = std::stoi(hex, nullptr, 16);
-            if (decimal >= 32 && decimal <= 127) {
+            if (decimal >= 32 && decimal < 128) {
                 std::string changed = s_20_7f[decimal - 32];
                 replace_all(decoded, "\\x" + hex, changed);
             }
